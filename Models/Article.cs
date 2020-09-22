b604dacab50206f24c17573666a11c1c0045f49e
Jack <jackwoodpearce@gmail.com>
2020-09-22 16:05:17 +0100

    Create Article lookup and loop through to display on index page

---
+++ Models/Article.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace blazorcore.Models
+{
+
+    public class Article
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string TickerCode { get; set; }
+        public string TickerFullName { get; set; }
+
+    }
+
+}
